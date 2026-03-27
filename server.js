4ef7c8659b2e395ac49329f400297186e67aa9e4
Potato Chips Website <user@example.com>
2026-03-27 11:58:39 +0530

    fix server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+// Root route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
+});
+
+// Use PORT for Render
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
